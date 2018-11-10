75d598373b2258918f765f3b03a024ac93a2419b
Semenov Evgeny <semenovem@gmail.com>
2018-11-10 09:12:18 +0300

    upd raspberry: readme.md bash for prep cd-card with OS for new raspberry

--- raspberrypi/readme.md
+++ raspberrypi/readme.md
@@ -1,3 +1,6 @@
+[official site raspberry pi](https://www.raspberrypi.org)
+[downloads OS RASPBIAN](https://www.raspberrypi.org/downloads/raspbian)
+
 
 application for iOS
 https://itunes.apple.com/ru/app/vnc-viewer/id352019548?mt=8
---
+++ raspberrypi/write_os_on_cd_card.sh
@@ -0,0 +1,16 @@
+# write image (*.dmg | *.img) at cd card for raspberry pi
+# 2018-11-09
+diskutil list
+
+exit 0
+
+#example path to drive - /dev/disk3
+
+diskutil unmountDisk /dev/disk3
+
+sudo newfs_msdos -F 32 /dev/disk3
+
+# ~/Downloads/2018-10-09-raspbian-stretch-lite.img path to image
+sudo dd if=~/Downloads/2018-10-09-raspbian-stretch-lite.img of=/dev/disk3
+
+
