9f11ff5a7fe2f446a6f5b81470c9de69ff0a1041
Semenov <semenovem@gmail.com>
2020-09-08 13:07:02 +0300

    linux

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -4,3 +4,12 @@ exit 0
 
 # redirecting logs to a file
 docker logs containername >& logs/myFile.log
+
+# how much space does docker take
+docker system df
+
+
+# Manage Docker as non-root user
+# ubuntu
+sudo groupadd docker
+sudo usermod -aG docker "$USER"
