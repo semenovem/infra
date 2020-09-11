a96d3c4bfeafa81951cf56505aef7361ab248c0e
Semenov <semenovem@gmail.com>
2020-09-11 21:21:03 +0300

    macos: mkfile

--- macos/cmd.sh
+++ macos/cmd.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
 
+exit
 
+# shows cpu temperature
 sudo powermetrics | grep -i "CPU die temperature"
+
+
+# creates a file of the specified size
+# maximum file size for FAT32 - 4,294,967,295b ( or 4G - 1 )
+mkfile 1g test.abc
+
+dd if=/dev/zero of=output.dat bs=10m
