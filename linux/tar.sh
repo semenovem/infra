1ead79b3f5acf0bc7709e9696fd149e010c2b55d
Semenov <semenovem@gmail.com>
2020-10-01 13:53:32 +0300

    tar

--- linux/tar.sh
+++ linux/tar.sh
@@ -4,4 +4,10 @@ exit
 
 # https://losst.ru/arhivatsiya-v-linux
 
+# create
+tar -zcvf archive.tar.gz /path/to/files
+
+# extract
+tar -zxvf archive.tar.gz
+
 
---
+++ macos/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# iterm
+
+#
