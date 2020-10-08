e8761a95b5fd19225997538f98e2fe4dccb43328
Semenov <semenovem@gmail.com>
2020-10-08 21:11:50 +0300

    docker swarm

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -13,3 +13,8 @@ docker system df
 # ubuntu
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
+
+
+
+# docker swarm
+docker node ls -q | xargs docker node inspect   -f '{{ .ID }} [{{ .Description.Hostname }}]: {{ .Spec.Labels }}'
