9ace85e1c0ce840e0c6fb423b2df36a918daa14e
semenov evgeny <emsemenov@vtb.ru>
2021-02-20 22:01:09 +0300

    bash

--- linux-like/tar.sh
+++ linux-like/tar.sh
@@ -10,4 +10,23 @@ tar -zcvf archive.tar.gz /path/to/files
 # extract
 tar -zxvf archive.tar.gz
 
+# -----------------
+# -----------------
+# -----------------
 
+
+#!/bin/bash
+
+LS=$(ls)
+for entry in $LS; do
+  if [ ! -f $entry ]; then
+    continue
+  fi
+  if [ "${entry: -3}" == ".sh" ]; then
+    continue
+  fi
+
+  echo $entry
+
+  zip -P 000000 "qqw/$entry.zip" $entry
+done
