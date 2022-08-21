7bd71ca867efe46d88f36c2b7a2c25a76cd04703
Semenov Evgeny <semenovem@gmail.com>
2022-08-21 21:18:20 +0300

    bin core

--- bin/_core/logger.sh
+++ bin/_core/logger.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
 
-CORE_LOGGER_SYSTEM_NAME="self"
-__CORE_LOGGER_SUB_SYSTEM_NAME__=$(basename $0)
+CORE_LOGGER_NAME="self"
+CORE_LOGGER_USE_DATA=
+
+__CORE_LOGGER_SUB_SYSTEM_NAME__=$(basename "$0")
 
 core_logger_sub_system_name() {
-  [ -n "$__CORE_LOGGER_SUB_SYSTEM_NAME__" ] && echo "[$__CORE_LOGGER_SUB_SYSTEM_NAME__] "
+  output=
+  [ -n "$CORE_LOGGER_USE_DATA" ] && output="[$(date)]"
+  [ -n "$__CORE_LOGGER_SUB_SYSTEM_NAME__" ] && output="${output}[$__CORE_LOGGER_SUB_SYSTEM_NAME__]"
+  echo "$output"
+}
+
+core_logger_use_data() {
+  [ -n "$CORE_LOGGER_USE_DATA" ] && echo "[$(date)]"
 }
 
 __err__() {
-  echo "[$CORE_LOGGER_SYSTEM_NAME][ERRO] [$(date)] $(core_logger_sub_system_name)$*"
+  echo "[$CORE_LOGGER_NAME][ERRO]$(core_logger_sub_system_name) $*"
 }
 
 __warn__() {
-  echo "[$CORE_LOGGER_SYSTEM_NAME][WARN] [$(date)] $(core_logger_sub_system_name)$*"
+  echo "[$CORE_LOGGER_NAME][WARN]$(core_logger_sub_system_name) $*"
 }
 
 __info__() {
-  echo "[$CORE_LOGGER_SYSTEM_NAME][INFO] [$(date)] $(core_logger_sub_system_name)$*"
+  echo "[$CORE_LOGGER_NAME][INFO]$(core_logger_sub_system_name) $*"
 }
 
 __debug__() {
-  echo "[$CORE_LOGGER_SYSTEM_NAME][DEBU] [$(date)] $(core_logger_sub_system_name)$*"
+  echo "[$CORE_LOGGER_NAME][DEBU]$(core_logger_sub_system_name) $*"
 }
