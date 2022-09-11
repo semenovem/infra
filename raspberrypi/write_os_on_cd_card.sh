e5971460012b2813270b2cbe29611b0ef1ec53ca
Semenov Evgeny <semenovem@gmail.com>
2022-09-11 16:26:37 +0300

    raspb write image

--- raspberrypi/write_os_on_cd_card.sh
+++ raspberrypi/write_os_on_cd_card.sh
@@ -8,7 +8,7 @@ diskutil unmountDisk /dev/disk4
 sudo newfs_msdos -F 32 /dev/disk4
 
 # ~/Downloads/2018-10-09-raspbian-stretch-lite.img path to image
-sudo dd if=rasbp.img of=/dev/disk4
+sudo dd status=progress if=rasbp.img of=/dev/disk4
 
 # ctr + t - show status
 # wait about ~15-30 min
