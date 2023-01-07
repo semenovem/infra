b808e1ad2b5166b79887f8898070a8c1bc78d108
Semenov Evgeny <semenovem@gmail.com>
2023-01-08 01:09:30 +0300

    torrent

--- bin/_utils/torrent/run.sh
+++ bin/_utils/torrent/run.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-CONFIG="${PWD}/config"
-TORRENTS="${PWD}/torrents"
+ROOT=$(dirname "$(echo "$0" | grep -E "^/" -q && echo "$0" || echo "$PWD/${0#./}")")
 
-DOWNLOADS="${PWD}/downloads"
-DOWNLOADS="/Volumes/dat/torrents"
+CONFIG_DIR="${ROOT}/config"
+CONFIG_FILE="${ROOT}/config.env"
+TORRENTS_DIR="${ROOT}/torrents"
+DOWNLOADS_DIR="/Volumes/dat/torrents"
 
-[ ! -d "$DOWNLOADS" ] \
-  && echo "download directory does not exist: '${DOWNLOADS}'" \
+if [ ! -f "$CONFIG_FILE" ]; then
+  {
+    echo "# В этом файле можно переопределить значения переменных"
+    echo
+    echo "DOWNLOADS_DIR=${DOWNLOADS_DIR}"
+  } >"$CONFIG_FILE"
+fi
+
+set -o allexport
+# shellcheck disable=SC1090
+. "$CONFIG_FILE" || exit 1
+set +o allexport
+
+[ ! -d "$DOWNLOADS_DIR" ] \
+  && echo "download directory does not exist: '${DOWNLOADS_DIR}'" \
   && exit 1
 
 
@@ -15,9 +29,9 @@ docker run -d --rm \
     --name torrent \
     --user "${UID}:${GID}" \
     -p 8080:8080 -p 6881:6881/tcp -p 6881:6881/udp \
-    -v "${CONFIG}:/config" \
-    -v "${TORRENTS}:/torrents" \
-    -v "${DOWNLOADS}:/downloads" \
+    -v "${CONFIG_DIR}:/config" \
+    -v "${TORRENTS_DIR}:/torrents" \
+    -v "${DOWNLOADS_DIR}:/downloads" \
     --add-host "bt.t-ru.org:185.15.211.203" \
     --add-host "bt2.t-ru.org:185.15.211.203" \
     --add-host "bt3.t-ru.org:185.15.211.203" \
