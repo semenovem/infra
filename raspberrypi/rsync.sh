f1a28327f1be7d3c06bb4f203ce817ec99740cf8
Semenov Evgeny <semenovem@gmail.com>
2023-03-25 16:04:43 +0300

    torren fix incomplete dir

--- raspberrypi/rsync.sh
+++ raspberrypi/rsync.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-rsync -azP --bwlimit=1000 --delete \
+# --bwlimit=1000
+
+rsync -azP --delete \
   --exclude="virtualbox/" --exclude=".*DS_Store" --exclude="._*" --exclude="*.!qB" \
   --progress /mnt/ext2t02/share backup-user@192.168.11.100:/mnt/hdd-2t/backup-mini-ext2t02
 
