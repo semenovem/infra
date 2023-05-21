b68ecb0dda84a776729b14b2fd6a4616a2c8af53
Semenov Evgeny <semenovem@gmail.com>
2023-05-21 16:46:36 +0000

    backup script

---
+++ dc/home/crone/backup.folder.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Копия каждый день в 23 часа
+# crontab
+# 1 23 * * * sh /home/evg/_environment/dc/home/crone/backup.folder.sh
+# 1 16 30 * * sh /home/evg/_environment/dc/home/crone/backup.folder.sh
+
+# sudo grep --color -i cron /var/log/syslog
+
+DAY=$(date +%m%d)
+INCREMENT_DIR="/mnt/hard/ya-disk/backups/glazkov/alex-incr/${DAY}"
+SRC_DIR="/mnt/hard/ya-disk/ИП Глазков/"
+DST_DIR="/mnt/hard/ya-disk/backups/glazkov/alex/"
+LOG_FILE="/home/evg/_envi_state/cron-glazkov-alex.log"
+
+echo "$(date) incremental backup (rsync) start with dir [${INCREMENT_DIR}]" >>"$LOG_FILE"
+if [ -d "$INCREMENT_DIR" ]; then
+  {
+    echo "$(date) incremental dir [${INCREMENT_DIR}] already exist"
+  } >>"$LOG_FILE"
+fi
+
+#if [ -e "$INCREMENT_DIR" ] ; then
+#  rm -rf "$INCREMENT_DIR"
+#fi
+
+rsync -a --delete --log-file "$LOG_FILE" --quiet --inplace --backup --backup-dir="$INCREMENT_DIR" "$SRC_DIR" "$DST_DIR"
