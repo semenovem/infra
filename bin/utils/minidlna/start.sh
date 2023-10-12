3e3ecdc6e843232d7f0c0cffc47601ed5b2f1d0c
Semenov Evgeny <semenovem@gmail.com>
2023-10-12 03:59:27 +0000

    debbug minidlna container

--- bin/utils/minidlna/start.sh
+++ bin/utils/minidlna/start.sh
@@ -8,17 +8,34 @@ DOCKER_IMAGE="infra/minidlna:1.0"
 CONTAINER_NAME="minidlna"
 DOCKER_FILE="${ROOT}/minidlna.dockerfile"
 PORT="8200"
-
+CONFIG_MEDIA="${HOME}/.infra/minidlna/media.conf"
 
 case "$1" in
 "start")
   __core_build_docker_image_if_not__ "$DOCKER_IMAGE" "$DOCKER_FILE" "$ROOT" || exit 1
 
+#  TMP_CONFIG_FILE=$(mktemp) || exit 1
+#  cat "${ROOT}/minidlna.conf" > "$TMP_CONFIG_FILE" || exit 1
+  ARGS="-v '/mnt/hdd-2t/torrent:/media:ro'"
+
+
+  for dir in $(grep -Ev '^[#[:space:]]+|^$' "$CONFIG_MEDIA"); do
+    echo ">>>>>>>>>>>> media_dir=/media/$dir"
+  done
+
+# set - -- -v "/mnt/hdd-2t/torrent:/media:ro"
+ set -- $ARGS
+# ls "$@"
+
+#echo "?????????? $@"
+##
+#  exit
+
   docker run -it --rm --name "$CONTAINER_NAME" \
     -u "nobody:nobody" \
     -p "${PORT}:8200" \
     -v "${ROOT}/minidlna.conf:/minidlna-dir/minidlna.conf:ro" \
-    -v "/Volumes/share/torrent:/media:ro" \
+    $@ \
     "$DOCKER_IMAGE" sh
 
   #  minidlnad -f "/minidlna-dir/minidlna.conf" -P "/tmp/minidlna.pid" -R -r
@@ -32,4 +49,3 @@ case "$1" in
 esac
 
 exit 0
-
