3283a08ebbc694f67a9c71ff70f26114d4a34bbe
Evgeny Semenov <semenovem@gmail.com>
2023-10-15 16:11:12 +0000

    debug remote

--- dc/office/run-fush.sh
+++ dc/office/run-fush.sh
@@ -2,6 +2,8 @@
 
 ROOT=$(dirname "$(echo "$0" | grep -E "^/" -q && echo "$0" || echo "$PWD/${0#./}")")
 
+cp "${ROOT}/index.html" "/home/evg/media" || exit 1
+
 docker run -d --restart on-failure:10 --name "screen-saver-fish" -p "8888:80" \
-  -v "${ROOT}/index.html:/usr/share/nginx/html:ro" \
+  -v "/home/evg/media:/usr/share/nginx/html:ro" \
   nginx:1.25.2
