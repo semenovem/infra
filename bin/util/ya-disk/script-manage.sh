b07ce7e9308ad5e70b7df3220c21c8fee41b173e
Semenov Evgeny <semenovem@gmail.com>
2023-10-23 03:41:45 +0000

    ya disk

--- bin/util/ya-disk/script-manage.sh
+++ bin/util/ya-disk/script-manage.sh
@@ -7,8 +7,11 @@ ROOT=$(dirname "$(echo "$0" | grep -E "^/" -q && echo "$0" || echo "$PWD/${0#./}
 
 IMAGE="infra/ya-disk:0.1"
 CONTAINER_NAME="ya-disk"
-DISK_DIR="/mnt/raid4t_hard/ya-disk"
 YA_CONFIG_DIR="${HOME}/.config/yandex-disk"
+
+NUMBER=1
+LOCAL_CONFIG_DIR="${__CORE_STATE_DIR__}/ya-disk"
+LOCAL_CONFIG_FILE=
 
 # Сборка образа
 __core_has_docker_image__ "$IMAGE"
@@ -31,11 +34,27 @@ fi
 
 help() {
   __info__ "Use arguments: [status|stop|start]"
+  __info__ "Local config: [${LOCAL_CONFIG_DIR}]"
 }
+
+if [ ! -d "$LOCAL_CONFIG_DIR" ]; then
+  mkdir "$LOCAL_CONFIG_DIR" || exit 1
+
+  {
+    echo "# Example config"
+    echo "# Directories to exclude (do not download content)"
+    echo "__EXCLUDE__="
+    echo "# Authorization data"
+    echo "__AUTH__="
+    echo "# Data"
+    echo "__YA_DISK_DIR__="
+  } > "${LOCAL_CONFIG_DIR}/example.conf"
+
+fi
 
 # shellcheck disable=SC2120
 is_running() {
-  HAS=$(docker ps --filter=name="$CONTAINER_NAME" -q $@)
+  HAS=$(docker ps --filter=name="$CONTAINER_NAME" -q "$@")
   [ -n "$HAS" ]
 }
 
@@ -47,12 +66,16 @@ for p in "$@"; do
   "status") OPER="status" ;;
   "stop") OPER="stop" ;;
   "start") OPER="start" ;;
-  *"help" | *"h")
+  "2") NUMBER="2" ;;
+  "help" | "h")
     help
     exit 0
     ;;
   esac
 done
+
+CONTAINER_NAME="${CONTAINER_NAME}-${NUMBER}"
+LOCAL_CONFIG_FILE="${LOCAL_CONFIG_DIR}/${NUMBER}.conf"
 
 # Действие
 case $OPER in
@@ -91,6 +114,44 @@ case $OPER in
     docker rm "$CONTAINER_NAME" || exit 1
   fi
 
+  if [ ! -f "$LOCAL_CONFIG_FILE" ]; then
+    __warn__ "local config [${LOCAL_CONFIG_FILE}] not exists"
+    exit 1
+  fi
+
+  # shellcheck disable=SC1090
+  . "$LOCAL_CONFIG_FILE" || exit 1
+
+  # создать конфиг - директорию диска __AUTH__
+
+  echo ">>>>>>> $__EXCLUDE__"
+  echo ">>>>>>> $__YA_DISK_DIR__"
+  echo ">>>>>>> $__AUTH__"
+  echo ">>>>>>> $YA_CONFIG_DIR"
+
+
+  docker run -it --rm \
+    --name "$CONTAINER_NAME" \
+    -w /ya \
+    -u "$(id -u):$(id -g)" \
+    --memory=500m \
+    --memory-swap=500m \
+    --cpus=0.3 \
+    -v "${__YA_DISK_DIR__}:/ya/disk:rw" \
+    -v "${ROOT}/config.cfg:/home/app/.config/yandex-disk/config.cfg:rw" \
+    -v "${__AUTH__}:/home/app/.config/yandex-disk:rw" \
+    "$IMAGE"  bash
+
+    exit
+
+    yandex-disk start \
+    --no-daemon \
+    --dir=/ya/disk \
+    --exclude-dirs=__only_cloud,__only_cloud2
+
+
+  exit
+
   docker run --detach --restart unless-stopped --platform=linux/amd64 \
     --name "$CONTAINER_NAME" \
     -w /ya \
@@ -98,7 +159,7 @@ case $OPER in
     --memory=500m \
     --memory-swap=500m \
     --cpus=0.3 \
-    -v "${DISK_DIR}:/ya/disk:rw" \
+    -v "${__YA_DISK_DIR__}:/ya/disk:rw" \
     -v "${ROOT}/config.cfg:/home/app/.config/yandex-disk/config.cfg:rw" \
     -v "${YA_CONFIG_DIR}:/home/app/.config/yandex-disk:rw" \
     "$IMAGE" yandex-disk start \
@@ -116,7 +177,7 @@ case $OPER in
   ;;
 
 *)
-  __warn__ "Command not passed"
+  __info__ "Command not passed"
   help
   ;;
 
