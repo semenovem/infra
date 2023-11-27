6b9b6feb577686ac379901cb63b84810daab08bf
Semenov Evgeny <semenovem@gmail.com>
2023-11-27 23:44:30 +0300

    scan local host

---
+++ bin/_lib/os.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#
+# Определение OS
+# $1 = hard вернуть macos / linux
+#
+
+which uname >/dev/null
+[ $? -ne 0 ] && echo "uname not found" >&2 && exit 1
+
+uname -s | grep -iEq "^Darwin"
+if [ $? -eq 0 ]; then
+  OUT="macos"
+  [ ! $1 = "hard" ] && OUT="${OUT}-$(uname -m)"
+  echo "$OUT"
+  exit 0
+fi
+
+[ $1 = "hard" ] && echo "linux" && exit 0
+
+which "/usr/bin/raspi-config" >/dev/null
+[ $? -eq 0 ] && echo "raspbian" && exit 0
+
+which "apt" >/dev/null
+[ $? -eq 0 ] && echo "debian" && exit 0
+
+which "apt-get" >/dev/null
+[ $? -eq 0 ] && echo "debian" && exit 0
+
+which "rpm" >/dev/null
+[ $? -eq 0 ] && echo "fedora" && exit 0
+
+which "yum" >/dev/null
+[ $? -eq 0 ] && echo "fedora" && exit 0
