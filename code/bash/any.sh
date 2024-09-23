2caafb96120151d176805fe507ec3224e0721a3f
Semenov Evgeny <semenovem@gmail.com>
2024-09-23 18:52:56 +0000

    adds bash code

--- code/bash/any.sh
+++ code/bash/any.sh
@@ -3,4 +3,9 @@
 export $(cat ./file.env | sed 's/#.*//g' | xargs)
 
 
-_BIN_=$(dirname "$([ "$0" = '/*' ] && echo "$0" || echo "$PWD/${0#./}")")
+_BIN_=$(dirname "$(echo "$0" | grep -E "^/" -q && echo "$0" || echo "$PWD/${0#./}")")
+
+
+# получить и удалить последний аргумент функции
+LAST_ELEM_CMD="${@: -1}"
+set -- "${@:1:$(($#-1))}"
