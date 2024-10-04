06f324586c0c9af937306f56642de8fd5664070e
Semenov Evgeny <semenovem@gmail.com>
2024-10-04 02:15:09 +0000

    testing nginx

---
+++ proj/endpoint/nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ROOT=$(dirname "$(echo "$0" | grep -E "^/" -q && echo "$0" || echo "$PWD/${0#./}")")
+
+echo "run nginx"
+
+docker run -it --rm \
+  -p 80:80 \
+  -p 443:443 \
+  -v "${ROOT}/conf:/etc/nginx/conf.d:ro" \
+  -v "${ROOT}/../../.local/certbot/www:/var/www/certbot:ro" \
+  -v "${ROOT}/../../.local/certbot/conf/:/etc/nginx/ssl:ro" \
+  nginx:1.27.1-bookworm-perl bash
+
+
---
+++ proj/endpoint/readme.md
@@ -0,0 +1,7 @@
+
+
+https://wiki.archlinux.org/title/Nginx_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)#%D0%91%D0%BB%D0%BE%D0%BA%D0%B8_server
+
+
+
+apt update && apt -y install certbot python3-certbot-nginx
