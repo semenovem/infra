454f6d36db8aa5603abc5fe149bf3638e23d8131
semenovem <semenovem@gmail.com>
2024-10-16 10:49:06 +0300

    sync dirs format

--- bin/infra-sync.sh
+++ bin/infra-sync.sh
@@ -44,7 +44,7 @@ while IFS= read -r line_raw; do
   REMOTE_PATH=$(echo "$line" | awk '{print $4}')
 
   if [ -z "$REMOTE_HOST" ] || [ -z "$PERIOD_HOUR" ] || [ -z "$LOCAL_PATH" ] || [ -z "$REMOTE_PATH" ]; then
-    echo "[ERRO] invalid line [${line_raw}]"
+    __err__ "[ERRO] invalid line [${line_raw}]"
     continue
   fi
 
@@ -59,45 +59,36 @@ while IFS= read -r line_raw; do
   if [ "$DIFF" -gt "$((PERIOD_HOUR * 3600))" ]; then
     # проверить доступность сервера
     ssh -qn "$REMOTE_HOST" > /dev/null
-    [ "$?" -ne 0 ] && echo "[ERRO] remote host unavailable [${REMOTE_HOST}]" && continue
+    [ "$?" -ne 0 ] && __err__ "[ERRO] remote host unavailable [${REMOTE_HOST}]" && continue
 
     LAST_SYNC_FILE="${LAST_SYNC_BASE}-${SERIAL}"
     break
   fi
-
-#  echo "SERIAL=${SERIAL}  REMOTE_HOST=${REMOTE_HOST}   PERIOD_HOUR=${PERIOD_HOUR}  LOCAL_PATH=${LOCAL_PATH}  REMOTE_PATH=${REMOTE_PATH}"
-#  echo "SERIAL=${SERIAL}  REMOTE_HOST=${REMOTE_HOST}  PERIOD_SEC=$((PERIOD_HOUR * 3600))  DIFF=${DIFF}  LOCAL_PATH=${LOCAL_PATH}   LAST_TIME=${LAST_TIME}  UNIX_SEC=${UNIX_SEC} IS_NEED_UPDATE=${IS_NEED_UPDATE}"
-
-#  echo "Text read from file: $line"
 done < "$SYNC_CONF_FILE"
 
 # -------------------
-[ -z "$LAST_SYNC_FILE" ] && echo "[INFO] nothing to update" && exit 0
+[ -z "$LAST_SYNC_FILE" ] && __info__ "[INFO] nothing to update" && exit 0
 
-
-echo "[INFO] REMOTE_HOST=${REMOTE_HOST}"
-echo "[INFO] PERIOD_HOUR=${PERIOD_HOUR}"
-echo "[INFO] LOCAL_PATH =${LOCAL_PATH}"
-echo "[INFO] REMOTE_PATH=${REMOTE_PATH}"
-echo "[INFO] SERIAL     =${SERIAL}"
-echo "[INFO] LAST_SYNC_FILE     =${LAST_SYNC_FILE}"
-
-__confirm__ "run sync ?" || exit 0
-
-# TODO добавить очистку файла лога перед синком, если лог превышает > 1mb
+__info__ "[INFO] REMOTE_HOST    = ${REMOTE_HOST}"
+__info__ "[INFO] PERIOD_HOUR    = ${PERIOD_HOUR}"
+__info__ "[INFO] LOCAL_PATH     = ${LOCAL_PATH}"
+__info__ "[INFO] REMOTE_PATH    = ${REMOTE_PATH}"
+__info__ "[INFO] SERIAL         = ${SERIAL}"
+__info__ "[INFO] LAST_SYNC_FILE = ${LAST_SYNC_FILE}"
 
 MONTH=$(date +%m)
 DAY_HOUR_MIN=$(date +%d%H%M)
 INCREMENT_DIR="${REMOTE_PATH}/$(basename "$LOCAL_PATH")-incr/${MONTH}/${DAY_HOUR_MIN}"
+__info__ "[INFO] $(date '+%Y-%m-%d %H:%M') incremental backup (rsync) start with dir [${INCREMENT_DIR}]"
 
-echo "[INFO] $(date) incremental backup (rsync) start with dir [${INCREMENT_DIR}]"
+__confirm__ "run sync ?" || exit 0
 
-rsync -a --delete --log-file=/dev/stdout --progress --inplace --backup --quiet \
+rsync -a --delete --log-file=/dev/stdout --inplace --backup --quiet \
   --rsync-path="mkdir -p ${REMOTE_PATH} && rsync" \
   --exclude '.git' \
   --exclude '.idea' \
   --exclude '.DS_Store' \
+  --exclude 'node_modules' \
   --backup-dir="$INCREMENT_DIR" "$LOCAL_PATH" "${REMOTE_HOST}:${REMOTE_PATH}"
-
 
 date +%s > "$LAST_SYNC_FILE"
