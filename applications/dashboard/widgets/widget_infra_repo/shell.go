5b0b00e6b0d67b8538492ef3ecaad19f46f59786
Semenov Evgeny <semenovem@gmail.com>
2024-11-10 18:55:37 +0300

    upd shell cmd

--- applications/dashboard/widgets/widget_infra_repo/shell.go
+++ applications/dashboard/widgets/widget_infra_repo/shell.go
@@ -2,12 +2,19 @@ package widget_infra_repo
 
 import (
 	"context"
+	"fmt"
 	"os/exec"
 	"strings"
 )
 
 func gitBranchName(ctx context.Context, path string) (string, error) {
-	cmd := exec.CommandContext(ctx, "git", "-C", path, "rev-parse", "--abbrev-ref", "HEAD")
+	cmd := exec.CommandContext(
+		ctx,
+		"sh",
+		"-c",
+		fmt.Sprintf("cd %s && git rev-parse --abbrev-ref HEAD", path),
+	)
+	//cmd := exec.CommandContext(ctx, "git", "-C", path, "rev-parse", "--abbrev-ref", "HEAD")
 
 	stdout, err := cmd.Output()
 	if err != nil {
