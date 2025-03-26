a050ea4aaefc43c052dfc8b008d2f1dae4b21bd2
Semenov Evgeny <semenovem@gmail.com>
2025-03-26 05:09:24 +0000

    up

--- dc/home/crone/backup.nextcloud.sh
+++ dc/home/crone/backup.nextcloud.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 
-# Копия каждый день в 23 часа
+# каждый день в 23 часа
 # crontab
 # 1 23 * * * sh /home/evg/infra/dc/home/crone/backup.folder.sh
 # 1 16 30 * * sh /home/evg/infra/dc/home/crone/backup.folder.sh
 
 # sudo grep --color -i cron /var/log/syslog
+mkdir -p /home/evg/logs
+mkdir -p /home/evg/proc
 
+LOG_DIR="/home/evg/logs"
+PREFIX="crone.nextcloud"
+PROC_FILE="/home/evg/proc/backup.nextcloud.pid"
 DAY=$(date +%m%d)
+
+if [ -f "$PROC_FILE" ]; then
+  PID="$(cat $PROC_FILE)" || exit
+
+  if [ -d "/proc/${PID}" ]; then
+    echo "[INFO][$(date +%m%d%H%m)] another instance is running PID=${PID}" >> "${LOG_DIR}/${PREFIX}_${DAY}_try"
+    exit 0
+  fi
+fi
+
+echo $$ > $PROC_FILE || exit
+
+
+echo ">>> $$"
+
+sleep 3600
+
+exit
+
+DST_BACKUP_DIR=/mnt/vol_media_1/backup_nextcloud
+
+
 
 # echo "$(date) incremental backup (rsync) start with dir [${INCREMENT_DIR}]" >>"$LOG_FILE"
 # if [ -d "$INCREMENT_DIR" ]; then
@@ -20,18 +47,22 @@ DAY=$(date +%m%d)
 #  rm -rf "$INCREMENT_DIR"
 #fi
 
-LOG_DIR="/home/evg/logs"
+
 
 SRC_EVG="/mnt/soft/nextcloud/app/data/evg/files/"
-DST_EVG="/mnt/vol_backup_1/backups_nextcloud/evg"
-DST_EVG_INCR=/"mnt/vol_backup_1/backups_nextcloud/evg-incr/${DAY}"
+DST_EVG="${DST_BACKUP_DIR}/evg"
+DST_EVG_INCR="${DST_BACKUP_DIR}/evg-incr/${DAY}"
 
 SRC_LEN="/mnt/soft/nextcloud/app/data/len/files/"
-DST_LEN="/mnt/vol_backup_1/backups_nextcloud/len"
-DST_LEN_INCR="/mnt/vol_backup_1/backups_nextcloud/len-incr/${DAY}"
+DST_LEN="${DST_BACKUP_DIR}/len"
+DST_LEN_INCR="${DST_BACKUP_DIR}/len-incr/${DAY}"
 
 
-rsync -a --delete --log-file="${LOG_DIR}/len.${DAY}.log" --inplace --backup --quiet \
+exit
+
+
+rsync -a --delete --log-file="${LOG_DIR}/${PREFIX}_len.${DAY}.log" \
+  --inplace --backup --quiet \
   --rsync-path="mkdir -p ${DST_LEN} && rsync" \
   --exclude '.git' \
   --exclude '.idea' \
