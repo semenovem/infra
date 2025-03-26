7156e04b4de48b421f366a64caecebc0a361b184
Evgeny <semenovem@gmail.com>
2025-03-26 22:08:44 +0300

    mini 47 adds ntp

---
+++ dc/mini47/ntp/init-ntp.sh
@@ -0,0 +1,20 @@
+
+apt-get install ntp
+vim /etc/ntp.conf
+
+systemctl status ntpsec.service 
+ntpq -p
+
+
+
+
+
+
+
+/etc/ntpsec/ntp.conf
+
+
+restrict − 4 default kod notrap nomodify nopeer noquery
+restrict − 6 default kod notrap nomodify nopeer noquery
+
+restrict 192.168.0.0 mask 255.255.0.0 nomodify notrap
