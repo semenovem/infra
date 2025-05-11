76dd1dde313982f0cfd1fdd231c00cd341e49135
Semenov Evgeny <semenovem@gmail.com>
2025-05-11 19:09:13 +0000

    adds template environments and rename global

--- bin/util/bot-evgio.sh
+++ bin/util/bot-evgio.sh
@@ -6,10 +6,10 @@ ROOT=$(dirname "$(echo "$0" | grep -E "^/" -q && echo "$0" || echo "$PWD/${0#./}
 . "${ROOT}/../../.local/services.env" || exit 1
 
 [ -z "$1" ] && echo "[ERRO] empty message in \$1" >&2 && exit 1
-[ -z "$BOT_API_EVGIO_CHAT_ID" ] && echo "[ERRO] empty BOT_API_EVGIO_CHAT_ID" >&2 && exit 1
-[ -z "$BOT_API_EVGIO_TOKEN" ] && echo "[ERRO] empty BOT_API_EVGIO_TOKEN" >&2 && exit 1
+[ -z "$__BOT_API_EVGIO_CHAT_ID__" ] && echo "[ERRO] empty __BOT_API_EVGIO_CHAT_ID__" >&2 && exit 1
+[ -z "$__BOT_API_EVGIO_TOKEN__" ] && echo "[ERRO] empty __BOT_API_EVGIO_TOKEN__" >&2 && exit 1
 
-# curl -s "https://api.telegram.org/bot${BOT_API_EVGIO_TOKEN}/getUpdates"
+# curl -s "https://api.telegram.org/bot${__BOT_API_EVGIO_TOKEN__}/getUpdates"
 
-curl -s -X POST "https://api.telegram.org/bot${BOT_API_EVGIO_TOKEN}/sendMessage" \
-  -d "chat_id=${BOT_API_EVGIO_CHAT_ID}&text=${1}"
+curl -s -X POST "https://api.telegram.org/bot${__BOT_API_EVGIO_TOKEN__}/sendMessage" \
+  -d "chat_id=${__BOT_API_EVGIO_CHAT_ID__}&text=${1}"
