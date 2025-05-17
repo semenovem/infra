29a0001b16286003d0c3237921eb0cb567a010a8
Semenov Evgeny <semenovem@gmail.com>
2025-05-17 17:08:09 +0000

    note about mini dockurr/win

--- dc/mini47/init.sh
+++ dc/mini47/init.sh
@@ -5,3 +5,13 @@ apt-get -y install autossh
 PasswordAuthentication no
 PermitRootLogin no
 AllowTcpForwarding yes
+
+
+
+docker run -it --rm --name windows -p 8006:8006 \
+  --device=/dev/kvm --device=/dev/net/tun \
+  --cap-add NET_ADMIN \
+  -v "/mnt/vol1/windows:/storage:rw"\
+  -v "/mnt/vol1/win11arm64.iso:/boot.iso:ro" \
+  --stop-timeout 120 \
+  dockurr/windows
