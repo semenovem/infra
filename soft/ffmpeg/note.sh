27363c0e6d670eb5ead3315fdc5c1af8e3881c3e
Semenov Evgeny <semenovem@gmail.com>
2025-06-13 15:49:26 +0000

    upd

--- soft/ffmpeg/note.sh
+++ soft/ffmpeg/note.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
 
+# 4k to fullHD
 
-ffmpeg -i 'Peaky.Blinders.s02e03.BDRemux.1080i.Rus.Eng.mkv' -vf "scale=1920:1080" -c:v libx264 -crf 23 -c:a copy '/home/evg/tmp/_videos/Peaky.Blinders.s02e03.mkv';
+SRC_DIR="/mnt/md1/for_resize_media/Peaky Blinders s03/"
+DST_DIR="/home/evg/tmp/_videos/Peaky_Blinders03"
+
+for ORIG_FILE in "${SRC_DIR}"*.mkv; do
+  RESIZED_FILE="${DST_DIR}/$(basename "$ORIG_FILE")"
+
+  echo ">>>>>>>>>>>>> name=$RESIZED_FILE"
+
+  ffmpeg -i "$ORIG_FILE" \
+    -vf "scale=1920:1080" -c:v libx264 -crf 23 -c:a copy -threads 4 \
+    "$RESIZED_FILE"
+done
+
+exit 0
+ 
