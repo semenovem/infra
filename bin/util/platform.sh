13e18f9aee7328dd0efc5c8687477991e1bf03ad
semenovem <semenovem@gmail.com>
2025-06-15 19:52:59 +0300

    util platform and temp

--- bin/util/platform.sh
+++ bin/util/platform.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# list of platforms
+# PLATFORM_RASPBIAN
+# PLATFORM_LINUX_UBUNTU
+# PLATFORM_LINUX_DEBIAN
+
+which "/usr/bin/raspi-config" >/dev/null && echo "PLATFORM_RASPBIAN" && exit 0
+
 case "$(uname)" in
-  'Linux') echo 'LINUX' ;;
-  'Darwin') echo 'MACOS_ARM' ;;
-*) echo "UNKNOWN";;
+  'Linux') echo 'PLATFORM_LINUX' ;;
+  'Darwin')
+    uname -a | grep -iq ARM64 && \
+    echo "PLATFORM_MACOS_ARM64" || echo 'PLATFORM_MACOS_INTEL'
+
+    exit 0
+  ;;
+*) echo "PLATFORM_UNKNOWN";;
 esac
